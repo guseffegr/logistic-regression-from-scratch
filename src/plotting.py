a01fbddcf7e6fa29d0ceb8d559538d4fe669cb8b
guseffegr <59287565+guseffegr@users.noreply.github.com>
2026-02-13 00:42:39 +0300

    Add files via upload

--- src/plotting.py
+++ src/plotting.py
@@ -10,7 +10,7 @@ def roc_curve_plot(y_val, y_proba):
     plt.plot([0, 1], [0, 1], linestyle="--", label="Random (AUC = 0.5)")
     plt.xlabel("False Positive Rate (FPR)")
     plt.ylabel("True Positive Rate (TPR)")
-    plt.title("ROC Curve (Validation set)")
+    plt.title("ROC Curve")
     plt.legend()
     plt.show()
 
@@ -22,6 +22,6 @@ def precision_recall_curve_plot(y_val, y_proba):
     plt.plot(recall_vals, precision_vals, label=f"PR curve (AUC = {pr_auc:.3f})")
     plt.xlabel("Recall")
     plt.ylabel("Precision")
-    plt.title("Precision–Recall Curve (Validation set)")
+    plt.title("Precision–Recall")
     plt.legend()
     plt.show()
